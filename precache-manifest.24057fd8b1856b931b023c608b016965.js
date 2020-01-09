6dad266cea55a655863192db7e5e67b0a451a405
VukSDev <vukspalajkovic@gmail.com>
2020-01-09 01:12:15 +0100

    Updates

---
+++ precache-manifest.24057fd8b1856b931b023c608b016965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25d84441724c1a4408d8621bda9fb24",
+    "url": "/HomePage/index.html"
+  },
+  {
+    "revision": "b36d7a416945280220de",
+    "url": "/HomePage/static/css/main.aa1114f5.chunk.css"
+  },
+  {
+    "revision": "e595eed646800760ada0",
+    "url": "/HomePage/static/js/2.acb3523d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/HomePage/static/js/2.acb3523d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b36d7a416945280220de",
+    "url": "/HomePage/static/js/main.7b90b80d.chunk.js"
+  },
+  {
+    "revision": "d90b2141c5fa15a8416d",
+    "url": "/HomePage/static/js/runtime-main.deb18d8b.js"
+  }
+]);
